2dd3901a52671eccb9e8412dee36f51fce9fc063
Szőllősi László <naintarlow@freemail.hu>
2019-04-25 08:34:28 +0200

    naintarlow

--- rest/src/main/java/cs8/web/controller/SzollosiLaszloController.java
+++ rest/src/main/java/cs8/web/controller/SzollosiLaszloController.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/szm")
+@RequestMapping("/api/szl")
 @Slf4j
 public class Szőllősi LászlóController {
 
