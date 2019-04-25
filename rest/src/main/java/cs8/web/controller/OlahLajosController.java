24d761391328978fe7d05451fc8c235b73ebed2d
Oláh Lajos <olalika98@gmail.com>
2019-04-25 09:19:14 +0200

    OlahLajos controller

---
+++ rest/src/main/java/cs8/web/controller/OlahLajosController.java
@@ -0,0 +1,21 @@
+package cs8.web.controller;
+
+import cs8.entity.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/api/ol")
+@Slf4j
+public class OlahLajosController {
+
+    @GetMapping("")
+    public User getUser() {
+        return new User("Oláh Lajos", "olalika98@gmail.com", LocalDateTime.now(), LocalDateTime.now());
+    }
+
+}
