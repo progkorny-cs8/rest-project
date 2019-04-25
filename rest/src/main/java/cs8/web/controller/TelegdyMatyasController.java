013ec2cde698942ec1e849a3a666c4ccd94c7def
matyicsibe <matyasbendeguz@gmail.com>
2019-04-25 09:43:22 +0300

    Átirtam a monogramomra

--- rest/src/main/java/cs8/web/controller/TelegdyMatyasController.java
+++ rest/src/main/java/cs8/web/controller/TelegdyMatyasController.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/szm")
+@RequestMapping("/api/tm")
 @Slf4j
 public class TelegdyMatyasController {
 
