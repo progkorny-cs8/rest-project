69a0fa902cdbb19ca65acd36d63634a153092ccd
matesszabo <matesszabo@hotmail.com>
2019-04-25 08:58:26 +0200

    rest projekt létrehozása archetype alapján

---
+++ rest/src/main/java/cs8/repo/UserRepository.java
@@ -0,0 +1,10 @@
+package cs8.repo;
+
+import cs8.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
