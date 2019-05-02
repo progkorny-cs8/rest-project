f10347de7119b54acc18c04f37b81cf4e8285b56
matesszabo <matesszabo@hotmail.com>
2019-05-02 08:40:14 +0200

    cs10 05.02

--- rest/src/main/java/cs8/web/controller/SzollosiLaszloController.java
+++ rest/src/main/java/cs8/web/controller/SzollosiLaszloController.java
@@ -1,4 +1,4 @@
-﻿package cs8.web.controller;
+package cs8.web.controller;
 
 import cs8.entity.User;
 import lombok.extern.slf4j.Slf4j;
@@ -7,17 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/szl")
 @Slf4j
-public class Szőllősi LászlóController {
+public class SzollosiLaszloController {
 
     @GetMapping("")
     public User getUser() {
         log.info("process=get-users");
-        return new User("Szőllősi László", "naintarlow@freemail.hu", LocalDateTime.now(), LocalDateTime.now());
+        return new User("Szöllősi László", "naintarlow@freemail.hu", LocalDateTime.now(), LocalDateTime.now());
     }
 
 
